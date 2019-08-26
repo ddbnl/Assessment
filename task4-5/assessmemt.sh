68958db3e2c60ba3e711c14017bee2d61129b670
Daan de Boer <daandeboer@gmail.com>
2019-08-26 10:14:37 +0200

    fix shell script

--- task4-5/assessmemt.sh
+++ task4-5/assessmemt.sh
@@ -11,7 +11,7 @@ echo "Getting files"
 git clone https://github.com/ddbnl/Assessment
 
 echo "Building image '$prefix/assessment'"
-docker build -t $prefix/assessment .
+docker build -t $prefix/assessment Assessment/task4-5/
 
 echo "Running image '$prefix/assessment'"
 docker run -p 8123:8123 --read-only --mount type=tmpfs,destination=/var/log/nginx,tmpfs-size=100000000 --mount type=tmpfs,destination=/var/lib/nginx,tmpfs-size=100000000 --mount type=tmpfs,destination=/var/cache/nginx,tmpfs-size=100000000 --mount type=tmpfs,destination=/tmp,tmpfs-size=100000000 $prefix/assessment
